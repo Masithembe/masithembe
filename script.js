41b9c2de45b8203e47c5aa49d776eea47bbdb1d5
masithembe <masithembegladile12@gmail.com>
2020-12-15 09:30:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+	 
+
+
+
+	 //SCROLL SKILLS
+	 sr.reveal('.skills__subtitle',{});
+	 sr.reveal('.skills__text',{});
+	 sr.reveal('.skills__data',{interval: 200});
+	 sr.reveal('.skills__img',{delay: 600});
+
+		  /*Responsive Navigation*/
+	  $(document).ready(function(){
+	 $('.toggle').click(function(){
+		 $('.toggle').toggleClass('active')
+		 
+		 $('nav ul').toggleClass('active-menu')
+		
+		 
+	 })
+ });
+	
+
+  
